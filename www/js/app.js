ab7fa18f6ac84d1ec98ee8196fd2ac5a8638feb6
ZEEF <zefmovement@gmail.com>
2019-10-02 08:14:45 +0200

    Init

---
+++ www/js/app.js
@@ -0,0 +1,171 @@
+// Dom7
+var $$ = Dom7;
+
+// Framework7 App main instance
+var app = new Framework7({
+  root: '#app', // App root element
+  id: 'io.framework7.testapp', // App bundle ID
+  name: 'Framework7', // App name
+  theme: 'auto', // Automatic theme detection
+  touch: {mdTouchRipple: false},
+  // App root data
+  data: function () {
+    return {
+      username: "...",
+      userAvatar: "../images/default.png",
+      userID: 1,
+      userEmail: "",
+      accountType: "",
+      loggedIn: false,
+      serverIP: "https://192.168.1.21/checkIn/",
+    };
+  },
+  // App root methods
+  methods: {
+    
+    popNavbar: function(){
+      let navbarwidth = $$('#navbar')[0].clientWidth;
+      document.getElementById('view-navbar').style.width = navbarwidth + "px";
+      document.getElementById('view-home').style.left = navbarwidth + "px";
+      $$('.usernamefield').html(app.data['username']);
+      $$('.profilepic').attr('src', app.data['userAvatar']);
+    },
+
+    highlightli: function(ID){
+      let navbarheader = $$('#navbar').find('li>a');
+      let navbarsubheader = $$('#navbar').find('li>div>div>a');
+      for(let i = 0; i < navbarheader.length; i++) {
+          if($$(navbarheader[i]).hasClass('active')){
+              $$(navbarheader[i]).removeClass('active');
+          }
+      }
+      for(let i = 0; i < navbarsubheader.length; i++) {
+        if($$(navbarsubheader[i]).hasClass('active')){
+            $$(navbarsubheader[i]).removeClass('active');
+        }
+    }
+
+      $$(`#${ID}`).addClass('active');
+    },
+
+    logout: function(){
+      app.data['username'] = "...";
+      app.data['userAvatar'] = "../images/default.png";
+      app.data['userID'] = 0;
+      app.data['loggedIn'] = false;
+      $$('#view-navbar').hide();
+    },
+
+    statusMsg: function (statusmsg, type, headerclass) {
+        let bkcolor;
+        if (type == "success") {
+            bkcolor = "#35a82a";
+        } else {
+            bkcolor = "#a82a2a";
+        }
+        let header = $$(headerclass);
+        header.html(statusmsg);
+        header.css({
+            'color': 'white',
+            'background-color': bkcolor
+        });
+        $$(header).parent().css('border-top-color', bkcolor);
+        setTimeout(function () {
+            header.html("Profile settings");
+            header.css({
+                'color': 'black',
+                'background-color': 'white'
+            });
+            $$(header).parent().css('border-top-color', '#2894ee');
+        }, 5000);
+        return;
+    },
+    
+    searchFunction: function (inputFieldID, tableID, rowCount) {
+
+      // En constant som er den funktion der skal køres
+      const search = (field, tableID, rowCount) => {
+        //Definer lets som der skal bruges i functionen
+        let input, filter, table, tr, td, txtValue;
+        //Sætter input variablen til at være vores input field object
+        input = field.target;
+        //Sætter det som skal bruges til at filtre i databasen til værende selve input feltets value
+        filter = input.value.toUpperCase();
+        //Sætter Table variablen til at være vores table object som skal søges i
+        table = $$(`#${tableID}`);
+        //Sætter tr variablen til at være alle de elemener i tabellen som matcher vores kriteria som er tr
+        tr = table.find('tr');
+        //Start på for loop for at kigge igennem alle tabel rows og finde noget der matcher
+        for (i = 0; i < tr.length; i++) {
+          //Sætter td til at være det specifikke element i tr som vi vil søge igennem
+          td = tr[i].getElementsByTagName("td")[rowCount];
+          //Checker om der faktisk er sat noget til td
+          if (td) {
+            //Finder den text value som er i td
+            txtValue = td.textContent || td.innerText;
+            //Checker om text valuen passer med noget af det vi søger efter
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              //Sætter rowets display til ikke at være none, så det faktisk er synligt
+              tr[i].style.display = "";
+            } else {
+              //Ellers sætter vi display til at være none, så det ikke kan ses af brugeren.
+              tr[i].style.display = "none";
+            }
+          }
+        }
+      }
+      
+      //Vi bruger dom7 til at vælge vores input felt, og så reagere vi på keyup events via et callback
+      $$(`#${inputFieldID}`).keyup(field => {
+        // Kald til funktionen men de krævede variabler
+        search(field, tableID, rowCount);
+      });
+      //Vi bruger dom7 til at vælge vores input felt, og så reagere vi på keyup events via et callback
+      $$(`#${inputFieldID}`).click(field => {
+        // Kald til funktionen men de krævede variabler
+        search(field, tableID, rowCount);
+      });
+    }, //Slut searchFunction() funktion
+    
+  },
+    // App routes
+    routes: routes,
+
+        on: {
+      pageInit: function (page) {
+        /*if(app.data['loggedIn'] == false){
+          document.getElementById('view-navbar').style.display = "none";
+          app.views.main.router.navigate("/login/", {
+            reloadCurrent: true, // Sikrer at der kommer friskt data på siden
+          });
+          return;
+        }*/
+        // Dette er for at sikre os at den første side der bliver indlæst af systemet, er udlon siden
+        let currentpage = app.views.main.router.currentPageEl.dataset.name;
+        if (currentpage == "home") {
+          app.views.main.router.navigate("/currentDay/", {
+            reloadCurrent: true, // Sikrer at der kommer friskt data på siden
+          });
+        };
+        },
+        pageAfterIn: function (page){
+          setTimeout(function () {
+            app.methods.popNavbar();
+          },25);
+        }
+      },
+  });
+
+// Init/Create views
+var homeView = app.views.create('#view-home', {
+  url: '/',
+  animate: false,
+  main: true,
+  master: true,
+});
+// Navbarview som er det view component der håndtere selve navbaren og alt hvad den gør
+var navbarView = app.views.create('#view-navbar', {
+  url: '/navbar/',
+  animate: false,
+  linksView: homeView,
+});
---
+++ www/js/routes.js
@@ -0,0 +1,143 @@
+routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    name: 'currentDay',
+    path: '/currentDay/',
+    componentUrl: './pages/currentday.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navcurrentDay");
+      }
+    }
+  },
+  {
+    name: 'overview',
+    path: '/overview/',
+    componentUrl: './pages/overview.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navoverview");
+      }
+    }
+  },
+  {
+    name: 'createuser',
+    path: '/createuser/',
+    componentUrl: './pages/createuser.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navcreateuser");
+      }
+    }
+  },
+  {
+    name: 'userlist',
+    path: '/userlist/',
+    componentUrl: './pages/userlist.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navuserlist");
+      }
+    }
+  },
+  {
+    name: 'unknowncardlist',
+    path: '/unknowncardlist/',
+    componentUrl: './pages/unknowncardlist.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navunknowncardlist");
+      }
+    }
+  },
+  {
+    name: 'createusergroup',
+    path: '/createusergroup/',
+    componentUrl: './pages/createusergroup.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navcreateusergroup");
+      }
+    }
+  },
+  {
+    name: 'groupoverview',
+    path: '/groupoverview/',
+    componentUrl: './pages/groupoverview.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navgroupoverview");
+      }
+    }
+  },
+  {
+    name: 'login',
+    path: '/login/',
+    componentUrl: './pages/login.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.logout();
+      }
+    }
+  },
+  {
+    name: 'usersoverview',
+    path: '/usersoverview/:groupID?/',
+    componentUrl: './pages/usersoverview.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navgroupoverview");
+      }
+    },
+    data: function(){
+      return{
+        projectID: routeTo.params.groupID, // Der laves en ny Variabel som indeholder de parametre som der blev sendt med routet
+      }
+    }
+  },
+  {
+    name: 'inspectuser',
+    path: '/inspectuser/:userID?/',
+    componentUrl: './pages/inspectuser.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navuserlist");
+      }
+    },
+    data: function(){
+      return{
+        projectID: routeTo.params.userID, // Der laves en ny Variabel som indeholder de parametre som der blev sendt med routet
+      }
+    }
+  },
+  {
+    name: 'inspectusertime',
+    path: '/inspectusertime/:userID?/',
+    componentUrl: './pages/inspectusertime.html',
+    on:{
+      pageInit: function (event, page){
+        app.methods.highlightli("navuserlist");
+      }
+    },
+    data: function(){
+      return{
+        projectID: routeTo.params.userID, // Der laves en ny Variabel som indeholder de parametre som der blev sendt med routet
+      }
+    }
+  },
+  {
+    name: 'navbar', // Internt navn på routen
+    path: '/navbar/', // Den sti som der skal kaldes når der routes i HTML koden, dvs der skal peges på denne sti i 'href'
+    componentUrl: './pages/navbar.html', // Den path hvor i filen som vi prøver at indlæse ligger i og navnet på filen
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    name: 'notfound',
+    path: '(.*)',
+    componentUrl: './pages/404.html',
+    'data-view': 'view-main',
+  },
+];
