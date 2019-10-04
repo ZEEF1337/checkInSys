b72493be794e2a434f6e37c3ad67b98e58b7cd2e
ZEEF <zefmovement@gmail.com>
2019-10-04 09:48:27 +0200

    postCreateScanner  + Added new endpoint for creating new scanners.

---
+++ endpoints/web/postCreateScanner.php
@@ -0,0 +1,55 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once ($_SERVER['DOCUMENT_ROOT']."/checkIn/functions/propFunctions.php");
+include_once ($_SERVER['DOCUMENT_ROOT']."/checkIn/database.inc");
+
+if(!isset($_POST['MAC']) || !isset($_POST['userID']) || !isset($_POST['name'])){
+    $out['result'] = 0;
+    $out['message'] = "Missing param";
+    $json = json_encode($out);
+    print_r($json);
+    return;
+}
+
+$givenName = $_POST['name'];
+$givenMAC = $_POST['MAC'];
+$userID = $_POST['userID'];
+
+$adminCheck = checkIfAdmin($userID);
+
+if($adminCheck == 0){
+    $out['result'] = 0;
+    $out['message'] = "No access";
+    $json = json_encode($out);
+    print_r($json);
+    return;
+}
+
+$database = new Database();
+$db = $database->getConnection();
+
+
+$query = "INSERT INTO scanners (MAC, Name) VALUES";
+$query .= " ('$givenMAC', '$givenName');";
+$stmt = $db->prepare($query);
+
+try{
+    $stmt->execute();
+    $out['result'] = 1;
+    $out['message'] = "Scanneren blev oprettet.";
+    
+} catch(PDOException $e){
+    print_r($e);
+    return;
+}
+
+$json = json_encode($out);
+print_r($json);
+return;
+?>
