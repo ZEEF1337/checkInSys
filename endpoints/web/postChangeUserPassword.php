39565b628db7214888f6528719f4fc586b023c5c
ZEEF <zefmovement@gmail.com>
2019-10-04 08:25:35 +0200

    postChangeUserPassword.php  + Added new endpoint to allow user to change own password.

---
+++ endpoints/web/postChangeUserPassword.php
@@ -0,0 +1,50 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once ($_SERVER['DOCUMENT_ROOT']."/checkIn/functions/propFunctions.php");
+include_once ($_SERVER['DOCUMENT_ROOT']."/checkIn/database.inc");
+
+
+$givenPassword = $_POST['password'];
+$givenOldPassword = $_POST['oldPassword'];
+$givenUserID = $_POST['userID'];
+
+$database = new Database();
+$db = $database->getConnection();
+
+$email = getEmailFromUserID($givenUserID);
+$verifyPass = verifyPass($email, $givenOldPassword);
+
+if($verifyPass == 0){
+    $out['result'] = 0;
+    $out['message'] = "Nuværende adgangskode forkert.";
+    $json = json_encode($out);
+    print_r($json);
+    return;
+}
+
+$salt = generateRandomSalt(32);
+$password = hash('sha512', hash('sha512', $salt).$givenPassword);
+
+$query = "UPDATE users SET Password = '$password', Salt = '$salt' WHERE ID = $givenUserID;";
+$stmt = $db->prepare($query);
+
+try{
+    $stmt->execute();
+    $out['result'] = 1;
+    $out['message'] = "Adgangskoden blev ændret.";
+    
+} catch(PDOException $e){
+    print_r($e);
+    return;
+}
+
+$json = json_encode($out);
+print_r($json);
+return;
+?>
